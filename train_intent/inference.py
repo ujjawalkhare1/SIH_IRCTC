13b23de51a22e00240ac28caa0935a6eab03809b
ujjawal-khare <ujjawal.khare@dream11.com>
2020-07-19 13:41:32 +0530

    enquiry updated

--- train_intent/inference.py
+++ train_intent/inference.py
@@ -3,13 +3,15 @@ from rasa_nlu.model import Trainer
 from rasa_nlu import config
 from rasa_nlu.model import Metadata, Interpreter
 from rasa_nlu.training_data  import load_data
+import time
 
-
-train_data = load_data('./test.json')
+print(time.time)
+train_data = load_data('./data/train_data1.json')
 
 trainer = Trainer(config.load("config.yml"))
 trainer.train(train_data)
-# model_directory = trainer.persist('/models/')
-# print(model_directory)
-# interpreter = Interpreter.load("./models/nlu/component_5_DIETClassifier.tf_model")
-# interpreter.parse(u"I am looking for an Italian Restaurant where I can eat")
+model_directory = trainer.persist('./models/')
+print(model_directory + " test")
+interpreter = Interpreter.load(model_directory)
+print("jj")
+interpreter.parse(u"i want to go from kanpur to delhi")
