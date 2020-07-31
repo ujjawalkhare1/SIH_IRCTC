23f3e3f42bd2be28b7171d229bcaa3bfb4cf8dfb
ujjawal-khare <ujjawal.khare@dream11.com>
2020-07-31 23:33:10 +0530

    intent added

--- EnquiryIntent/data/dataTrainNumber.py
+++ EnquiryIntent/data/dataTrainNumber.py
@@ -24,7 +24,6 @@ for i in range(iteration):
 		lenEntity = len(trainNumber)
 		endTrainNumberEntity = startTrainNumberEntity + lenEntity
 
-
 		sent= sent.lower()
 
 		fp.write('	{ \n')
@@ -44,6 +43,42 @@ for i in range(iteration):
 		fp.write('	}, \n')
 		# else:
 		# 	fp.write('	} \n')
+
+filepath1 = './pnrStatusData.txt'
+with open(filepath1,'r',encoding = 'utf-8') as infile1:
+    sents1 = infile1.read().strip().split('\n')
+
+for i in range(iteration):
+	for sent in sents1:
+		sent = sent + " "
+
+		pnrNumber = str(random.randrange(10001, 99999,4)) + str(random.randrange(10001, 99999,4))
+		# print(pnrNumber)
+		# print(sent + "semt")
+		sent = sent.replace('{pnr}', pnrNumber)
+		startPnrNumberEntity = sent.index(pnrNumber)
+		lenEntity = len(pnrNumber)
+		endPnrNumberEntity = startPnrNumberEntity + lenEntity
+
+		sent = sent.lower()
+
+
+		fp.write('	{ \n')
+		fp.write('	"text": "' + sent + '",\n')
+		fp.write('	"intent": "pnr number", \n')
+		fp.write('	"entities": [\n   { \n ')
+
+
+		fp.write('	"start" : ' + str(startPnrNumberEntity) + ', \n')
+		fp.write('	"end":' + str(endPnrNumberEntity) + ',\n')
+		fp.write('	"value":"' + pnrNumber + '",\n')
+		fp.write('	"entity": "pnrNumber" \n   }')
+
+
+		fp.write('\n')
+		fp.write('	]\n')
+		fp.write('	}, \n')
+
 
 fp.write(']' + '\n')
 fp.write('} \n')
